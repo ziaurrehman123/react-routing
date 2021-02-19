49789a7ef6bcac20155729dfa69da3fbcee39493
ziaurrehman123 <ziaurrehman1122111@gmail.com>
2021-02-19 15:54:33 +0500

    react routing implemented through react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+import Contact from './Contact'
+import About from './About'
+import Error from './Error'
+const App =()=>{
+  const Name =()=>{
+    return (
+      <h1>Hello I am name page</h1>
+    )
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+   <Switch>
+     <Route exact path="/" component={About}/>
+     <Route exact path="/contact" component={Contact}/>
+     <Route path="/contact/name" component={Name}/>
+     <Route component={Error}/>
+   </Switch>
+   {/* <About />
+   <Contact /> */}
+   </>
+  )
 }
 
 export default App;
---
+++ src/Error.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Error =()=>{
+  return (
+    <h1>
+     Oops! page not found :)
+    </h1>
+  )
+}
+
+export default Error;
